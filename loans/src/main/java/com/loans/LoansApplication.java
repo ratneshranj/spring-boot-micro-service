ed0ec0f288a264f38f496b799c8d244c3962691d
ratneshranj <ratneshranj@gmail.com>
2022-08-28 18:14:13 +0530

    added loans code

--- loans/src/main/java/com/loans/LoansApplication.java
+++ loans/src/main/java/com/loans/LoansApplication.java
@@ -2,8 +2,14 @@ package com.loans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.loans.controller")
+@EnableJpaRepositories("com.loans.repository")
+@EntityScan("com.loans.model")
 public class LoansApplication {
 
 	public static void main(String[] args) {
